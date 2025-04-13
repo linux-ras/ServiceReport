69d8855aa1e76a2381b175ed69016d5df81c6f08
Sourabh Jain <sourabhjain@linux.ibm.com>
2025-04-13 19:42:28 +0530

    [spyre] add sos report plugin
    Add a sos report plugin named spyre-external.py to the project.
    
    The sos spyre plugin will be installed under the sos report Python
    site-packages directory, along with other sos plugins.
    
    The new plugin collects information related to Spyre cards, including
    the following:
    
     - tree /sys/devices/pci/{spyre_pci_domain_bus}/{card}/vfio-dev
     - lsslot -c pci
     - lsslot -c phb
    
    The plugin is named spyre-external to avoid conflicts in the future if a
    spyre plugin is added by the distro's sos package.
    
    Note: No functional changes are intended for ServiceReport.
    
    Cc: Mahesh Salgaonkar <mahesh@linux.ibm.com>
    Signed-off-by: Sourabh Jain <sourabhjain@linux.ibm.com>

---
+++ sos-plugin/spyre-external.py
@@ -0,0 +1,73 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+#
+# This plugin enables collection of logs for system with IBM Spyre card
+
+import re
+import pyudev
+
+from sos.report.plugins import Plugin, IndependentPlugin
+
+
+class SpyreExternal(Plugin, IndependentPlugin):
+    """ Out of tree sos plugin to collect IBM Spyre
+    card related logs.
+    """
+
+    short_desc = 'IBM Spyre Accelerator Information'
+    plugin_name = 'spyre-external'
+    architectures = ('ppc.*',)
+
+    # IBM: 1014
+    # IDs must be in hex format
+    card_vendor_ids = ["0x1014"]
+    card_device_ids = ["0x06a7", "0x06a8"]
+
+    def setup(self):
+        spyre_cards = self.get_spyre_cards()
+
+        # Nothing to collect if spyre card is not present in the system
+        if not spyre_cards:
+            return
+
+        # Collects the VFIO device's sysfs directory structure
+        for card in spyre_cards:
+            match = re.match(r"(\w+:\w+):", card)
+            if not match:
+                continue
+
+            pci_domain_bus = match.group(1)
+
+            pci_vfio_dir = f"/sys/devices/pci{pci_domain_bus}/{card}/vfio-dev"
+            self.add_dir_listing(pci_vfio_dir, tree=True)
+
+        # Lists PCI and PHB (PCI Host Bridge) slots and their details
+        # on a Power system
+        self.add_cmd_output([
+            "lsslot -c pci",
+            "lsslot -c phb",
+        ])
+
+    def get_spyre_cards(self):
+        context = pyudev.Context()
+        spyre_cards_bus_ids = []
+
+        for device in context.list_devices(subsystem='pci'):
+            vendor_id = device.attributes.get("vendor").decode("utf-8").strip()
+            if vendor_id not in self.card_vendor_ids:
+                continue
+
+            device_id = device.attributes.get("device").decode("utf-8").strip()
+            if device_id not in self.card_device_ids:
+                continue
+
+            spyre_cards_bus_ids.append(device.sys_name)
+
+        return spyre_cards_bus_ids
+
+# vim: set et ts=4 sw=4 :
